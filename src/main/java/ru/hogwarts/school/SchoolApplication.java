b7cd3586a989cac154b924d20854994c0281967f
alexxaski <alexxaski@yandex.ru>
2025-03-14 12:13:49 +0300

    a1.3.7

--- src/main/java/ru/hogwarts/school/SchoolApplication.java
+++ src/main/java/ru/hogwarts/school/SchoolApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
-
-		System.out.println("привет");
 	}
+
+      System.out.println("привет
+
 }
 
