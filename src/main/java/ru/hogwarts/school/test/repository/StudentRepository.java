edbcbb34ad543c63157f37ac17255a6835763f50
alexxaski <alexxaski@yandex.ru>
2025-04-16 22:49:02 +0300

    sql-1.0

--- src/main/java/ru/hogwarts/school/test/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/test/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.school.test.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.test.model.Student;
 
@@ -10,4 +11,13 @@ import java.util.List;
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     List<Student> findByAgeBetween(int min, int max);
+
+    @Query("SELECT COUNT(s) FROM Student s")
+    Long countStudents();
+
+    @Query("SELECT AVG(s.age) FROM Student s")
+    Double averageAge();
+
+    @Query(value = "SELECT * FROM Student ORDER BY id DESC LIMIT 5", nativeQuery = true)
+    List<Student> findLastFiveStudents();
 }
